d4c7c204c8708c9e7d73f83c51566d8723ee1333
nunnunnana <ganada38@naver.com>
2022-04-10 17:20:46 +0900

    Initial commit

---
+++ src/test/java/Capstone/tancheon/TancheonApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.tancheon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TancheonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
