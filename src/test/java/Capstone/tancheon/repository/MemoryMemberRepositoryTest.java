084d0bb35ef32cdb330eaa747aa88d04fd7e2ce5
nunnunnana <ganada38@naver.com>
2022-11-08 22:54:55 +0900

    회원 조회 추가

---
+++ src/test/java/Capstone/tancheon/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package Capstone.tancheon.repository;
+
+import Capstone.tancheon.domain.Member;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        System.out.println("result = " + (result == member));
+        
+    }
+}
